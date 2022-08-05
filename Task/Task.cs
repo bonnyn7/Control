c17adc446279528c26855579ab2f35529f1a858e
Anastasia <bonnyn7@yandex.ru>
2022-08-05 16:39:33 +0300

    Added second part

--- Task/Task.cs
+++ Task/Task.cs
@@ -9,6 +9,18 @@
             SecondArray(firstArray, secondArray);
             PrintArray(secondArray);
         }
+        static void SecondArray(string[] firstArray, string[] secondArray)
+        {
+            int count = 0;
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                if (firstArray[i].Length <= 3)
+                {
+                    secondArray[count] = firstArray[i];
+                    count++;
+                }
+            }
+        }
         
     }
 }
