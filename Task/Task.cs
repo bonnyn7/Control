9c11a238126ff664b40a6201139ee9fdb2b5b12d
Anastasia <bonnyn7@yandex.ru>
2022-08-05 16:35:25 +0300

    Rename file Task

---
+++ Task/Task.cs
@@ -0,0 +1,14 @@
+﻿namespace Homework9
+{
+    class Task
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArray = new string[5] { "oak", ":-)", "pine", "maple", "apple" };
+            string[] secondArray = new string[firstArray.Length];
+            SecondArray(firstArray, secondArray);
+            PrintArray(secondArray);
+        }
+        
+    }
+}
