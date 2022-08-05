2002e2601ca64588c4cbbf0ce6b45243be37693a
Anastasia <bonnyn7@yandex.ru>
2022-08-05 16:48:07 +0300

    Changed file name

---
+++ Task/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Control
+{
+    class Task
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArray = new string[5] { "oak", ":-)", "pine", "maple", "apple" };
+            string[] secondArray = new string[firstArray.Length];
+            SecondArray(firstArray, secondArray);
+            PrintArray(secondArray);
+        }
+        static void SecondArray(string[] firstArray, string[] secondArray)
+        {
+            int count = 0;
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                if (firstArray[i].Length <= 3)
+                {
+                    secondArray[count] = firstArray[i];
+                    count++;
+                }
+            }
+        }
+        static void PrintArray(string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
